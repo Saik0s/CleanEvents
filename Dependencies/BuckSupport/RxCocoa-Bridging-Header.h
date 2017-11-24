a28926aff44736aaa605124800e13488e393d6c6
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2017-11-24 20:26:22 +0200

    :hammer: cleanup

--- Dependencies/BuckSupport/RxCocoa-Bridging-Header.h
+++ Dependencies/BuckSupport/RxCocoa-Bridging-Header.h
@@ -1,9 +1,4 @@
-// #import <UIKit/UIKit.h>
 #import "RxCocoa/Runtime/include/_RX.h"
 #import "RxCocoa/Runtime/include/_RXDelegateProxy.h"
 #import "RxCocoa/Runtime/include/_RXKVOObserver.h"
 #import "RxCocoa/Runtime/include/_RXObjCRuntime.h"
-// #import <RxCocoa/_RX.h>
-// #import <RxCocoa/_RXDelegateProxy.h>
-// #import <RxCocoa/_RXKVOObserver.h>
-// #import <RxCocoa/_RXObjCRuntime.h>
