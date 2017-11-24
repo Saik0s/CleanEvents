86f3e881bdd94fe5f38613c1ea32d0268c16288a
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2017-11-24 18:43:18 +0200

    :wrench: appdelegate in separate lib
    buck configs for core lib and app binary

--- Dependencies/BuckSupport/RxCocoa-Bridging-Header.h
+++ Dependencies/BuckSupport/RxCocoa-Bridging-Header.h
@@ -1,5 +1,9 @@
-#import <UIKit/UIKit.h>
+// #import <UIKit/UIKit.h>
 #import "RxCocoa/Runtime/include/_RX.h"
 #import "RxCocoa/Runtime/include/_RXDelegateProxy.h"
 #import "RxCocoa/Runtime/include/_RXKVOObserver.h"
 #import "RxCocoa/Runtime/include/_RXObjCRuntime.h"
+// #import <RxCocoa/_RX.h>
+// #import <RxCocoa/_RXDelegateProxy.h>
+// #import <RxCocoa/_RXKVOObserver.h>
+// #import <RxCocoa/_RXObjCRuntime.h>
--- UITests/Main.swift
+++ UITests/Main.swift
@@ -1,36 +1,35 @@
 //
-//  CakeManiaUITests.swift
-//  CakeManiaUITests
-//
-//  Created by Maxim Khatskevich on 11/25/16.
-//  Copyright © 2016 CakeMania Inc. All rights reserved.
+// Main.swift
+// Created by Igor Tarasenko on 18/11/17
+// Copyright © 2017 Igor Tarasenko
+// Licensed under the MIT license, see LICENSE file
 //
 
 import XCTest
 
 class CakeManiaUITests: XCTestCase {
-        
-    override func setUp() {
-        super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
+override func setUp() {
+super.setUp()
+
+// Put setup code here. This method is called before the invocation of each test method in the class.
+
+// In UI tests it is usually best to stop immediately when a failure occurs.
+continueAfterFailure = false
+// UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+XCUIApplication().launch()
+
+// In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+override func tearDown() {
+// Put teardown code here. This method is called after the invocation of each test method in the class.
+super.tearDown()
+}
+
+func testExample() {
+// Use recording to get started writing UI tests.
+// Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
 }
